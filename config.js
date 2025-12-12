4fc928cf87664e6b073999a5d4ba4e12017115a2
swatithak30 <thakswati90@gmail.com>
2025-12-12 18:14:10 +0530

    Add js

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+  apiUrl: "http://10.10.11.37:8080/CORE_2.2.2",
+  merchantlogo:"D:/VegaaHPlugins/PaymentMethodTemplate/JSCheckout/images/merchant-logo.png",
+  currency: "SAR"
+};
